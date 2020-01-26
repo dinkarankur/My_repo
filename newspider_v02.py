f57236f59b97427e538ef841837f1ee553d832b0
dinkarankur <59006566+dinkarankur@users.noreply.github.com>
2020-01-26 21:32:50 +0530

    Add files via upload
    some addition and substraction

---
+++ newspider_v02.py
@@ -0,0 +1,46 @@
+'''
+credited_to : 
+[@Author]'Ahmed Rafik Djerah in @'Medium.com'
+
+title : spider fundamentals
+writen by nyk
+'''
+import scrapy
+
+class newspider(scrapy.Spider):
+    name = 'newspider'
+    allowed_domain = ['www.gsmarena.com']
+    start_urls = ['https://www.gsmarena.com/makers.php3']
+    
+    def parse(self, response):
+        for link in response.xpath('//div[@class="st-text"]/table/tr/td/a/@href').extract():
+            yield response.follow(link, callback=self.parse_make_details)
+               
+        model = response.xpath('//div[@class="makers"]/ul/li/a/strong/span/text()').extract_first()
+        yield {
+            'Model' : model
+        }
+        for link in response.xpath('//div[@class="makers"]/ul/li/a/@href').extract():
+            yield response.follow(link, callback=self.parse_details)
+        next_page = response.xpath('//div[@class="nav-pages"]/a/@href').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+    
+    def parse_make_details(self, response):
+        make = response.xpath('//div[@class="st-text"]/table/tr/td/a/@href').extract_first()
+        yield {
+            'Make' : make  
+        }
+
+    def parse_details(self, response):
+        memory = response.xpath('//td[@data-spec="internalmemory"]/text()').extract_first()
+        camera = response.xpath('//td[@data-spec="cam1modules"]/text()').extract_first()
+        battery = response.xpath('//td[@data-spec="batdescription1"]/text()').extract_first()
+        price = response.xpath('//td[@data-spec="price"]/text()').extract_first()
+        yield {
+            'Memory' : memory,
+            'Camera' : camera,
+            'Battery' : battery,
+            'Price' : price
+            }
+
